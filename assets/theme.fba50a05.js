0994376d13fd12010768ede5bd11467f6f74969f
dwkeis-apmic <dwkeis@gmail.com>
2024-04-12 00:55:39 +0800

    feat: init;

---
+++ assets/theme.fba50a05.js
@@ -0,0 +1,1 @@
+import{D as t}from"./index.2fbfc771.js";const s=t("theme",{state:()=>({useLightTheme:!0}),getters:{getLightTheme:e=>e.useLightTheme},actions:{setLightTheme(e){this.useLightTheme=e}}});export{s as u};
