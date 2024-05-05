252c90a7b3c92159c3e70ac3300b8acde630297b
dwkeis-apmic <dwkeis@gmail.com>
2024-05-06 00:51:10 +0800

    feat: update docs;

--- assets/theme.7438f90c.js
+++
@@ -1,1 +0,0 @@
-import{D as t}from"./index.b7805b41.js";const s=t("theme",{state:()=>({useLightTheme:!0}),getters:{getLightTheme:e=>e.useLightTheme},actions:{setLightTheme(e){this.useLightTheme=e}}});export{s as u};
