8cb425a85e2756afb8eec7e30532c510413e2a8e
dwkeis <dwkeis@gmail.com>
2024-06-16 23:14:22 +0800

    feat: project page updated;

--- assets/theme.e98e268d.js
+++
@@ -1,1 +0,0 @@
-import{D as t}from"./index.c1c90181.js";const s=t("theme",{state:()=>({useLightTheme:!0}),getters:{getLightTheme:e=>e.useLightTheme},actions:{setLightTheme(e){this.useLightTheme=e}}});export{s as u};
